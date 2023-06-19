925632063265bc5b74e07a19d7c88f64bf58a5d8
AjayChintalapudi <chintalapudiajay.viljetech@gmail.com>
2023-06-19 13:18:12 +0530

    FolderStructureCompleted

---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from 'pages/Home/Home';
+import Events from 'pages/Events/Events';
+import About from 'pages/About/About';
+import Products from 'pages/Products/Products';
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/events" element={<Events />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoutes;
