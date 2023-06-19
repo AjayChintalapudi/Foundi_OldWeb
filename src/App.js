5cf879a74703dcb147403cd57e88b2767f59d2eb
KarthikDamanellore <karthikdamanellore.viljetech@gmail.com>
2023-06-19 20:25:58 +0530

    code:splitting for footer section completed.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Footer from 'components/Footer/Footer';
-import AppRoutes from 'routes/AppRoutes';
 
 function App() {
   return (
     <div className="App">
       {/* <AppRoutes /> */}
       <Footer />
-      <AppRoutes />
     </div>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import styles from './styles.module.css';
 import { Strings } from 'resources/Strings/eng';
 import Input from 'components/Input/Input';
 import Button from 'components/Button/Button';
 import { footerIconsData } from 'constants/CommonData/CommonData';
+import styles from './styles.module.css';
 
 const Footer = () => {
   const { footerStrings } = Strings;
@@ -11,22 +11,34 @@ const Footer = () => {
   const footerTopSection = () => {
     return (
       <div className={styles.footerTopSection}>
-        <div className={styles.footerTopSectionLeftBlock}>
-          <h3 className={styles.footerMainHeading}>
-            {footerStrings.footerMainHeading}
-          </h3>
-          <p className={styles.footerSubDesc}>{footerStrings.footerSubDesc}</p>
-        </div>
-        <div className={styles.footerTopSectionRightBlock}>
-          <div className={styles.footerInputContainer}>
-            <Input
-              customInputStyles={styles.footerInputStyles}
-              placeholder={footerStrings.footerPlaceHolderText}
-            />
-            <div className={styles.footerButtonContainer}>
-              <Button btName="submit" btnStyles={styles.footerBtnStyles} />
-              <Button btName=">" btnStyles={styles.footerBtnIconStyles} />
-            </div>
+        {footerTopSectionLeftBlock()}
+        {footerTopSectionRightBlock()}
+      </div>
+    );
+  };
+
+  const footerTopSectionLeftBlock = () => {
+    return (
+      <div className={styles.footerTopSectionLeftBlock}>
+        <h3 className={styles.footerMainHeading}>
+          {footerStrings.footerMainHeading}
+        </h3>
+        <p className={styles.footerSubDesc}>{footerStrings.footerSubDesc}</p>
+      </div>
+    );
+  };
+
+  const footerTopSectionRightBlock = () => {
+    return (
+      <div className={styles.footerTopSectionRightBlock}>
+        <div className={styles.footerInputContainer}>
+          <Input
+            customInputStyles={styles.footerInputStyles}
+            placeholder={footerStrings.footerPlaceHolderText}
+          />
+          <div className={styles.footerButtonContainer}>
+            <Button btName="submit" btnStyles={styles.footerBtnStyles} />
+            <Button btName=">" btnStyles={styles.footerBtnIconStyles} />
           </div>
         </div>
       </div>
@@ -36,27 +48,39 @@ const Footer = () => {
   const footerBottomSection = () => {
     return (
       <div className={styles.footerBottomSection}>
-        <div className={styles.footerBottomSectionLeftBlock}>
-          {footerIconsData &&
-            footerIconsData.map((item, index) => {
-              return (
-                <div key={index} className={styles.socialMediaImgBlock}>
-                  <img
-                    src={item.footerSocialMediaImg}
-                    alt={item.footerAltText}
-                    className={styles.imageWidth}
-                  />
-                </div>
-              );
-            })}
-        </div>
-        <div className={styles.footerBottomSectionRightBlock}>
-          <span className={styles.rightBorderDivider}></span>
-          <div className={styles.bottomBorder}></div>
-          <p className={styles.footerCopyRightDesc}>
-            {footerStrings.footerCopyRightDesc}
-          </p>
-        </div>
+        {footerBottomSectionLeftBlock()}
+        {footerBottomSectionRightBlock()}
+      </div>
+    );
+  };
+
+  const footerBottomSectionLeftBlock = () => {
+    return (
+      <div className={styles.footerBottomSectionLeftBlock}>
+        {footerIconsData &&
+          footerIconsData.map((item, index) => {
+            return (
+              <div key={index} className={styles.socialMediaImgBlock}>
+                <img
+                  src={item.footerSocialMediaImg}
+                  alt={item.footerAltText}
+                  className={styles.imageWidth}
+                />
+              </div>
+            );
+          })}
+      </div>
+    );
+  };
+
+  const footerBottomSectionRightBlock = () => {
+    return (
+      <div className={styles.footerBottomSectionRightBlock}>
+        <span className={styles.rightBorderDivider}></span>
+        <div className={styles.bottomBorder}></div>
+        <p className={styles.footerCopyRightDesc}>
+          {footerStrings.footerCopyRightDesc}
+        </p>
       </div>
     );
   };
