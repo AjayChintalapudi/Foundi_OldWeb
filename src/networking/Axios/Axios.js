7da72cdb06b8c6ccc11910a47992fad8307e4902
AjayChintalapudi <chintalapudiajay.viljetech@gmail.com>
2023-06-28 12:24:08 +0530

    getting userdata provider from main.

--- src/networking/Axios/Axios.js
+++ src/networking/Axios/Axios.js
@@ -1,11 +1,6 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-<<<<<<< HEAD
-  baseURL: 'http://192.168.29.109:3001/api/v1',
-});
-
-=======
   baseURL: 'http://192.168.29.238:3001/api/v1',
 });
 
@@ -13,7 +8,6 @@ axiosInstance.defaults.headers.common.Authorization = `Bearer ${localStorage.get
   'auth'
 )}`;
 
->>>>>>> 537e398826ec4aca8337112f8b54c4b87851ea00
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) =>
@@ -21,11 +15,4 @@ axiosInstance.interceptors.response.use(
       (error.response && error.response.data) || 'something went wrong'
     )
 );
-<<<<<<< HEAD
-axiosInstance.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem(
-  'auth'
-)}`;
-=======
-
->>>>>>> 537e398826ec4aca8337112f8b54c4b87851ea00
 export default axiosInstance;
--- src/networking/EndPoints/EndPoints.js
+++ src/networking/EndPoints/EndPoints.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 //login endpoint
 export const logIn = '/auth/login';
-=======
 // signUp  EndPoint
 
 export const signUp = '/auth/register';
->>>>>>> 537e398826ec4aca8337112f8b54c4b87851ea00
